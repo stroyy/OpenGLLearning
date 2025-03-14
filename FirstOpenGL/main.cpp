464e74e8269ccda2154dfc6242dc2ecb89d7eb70
yu <yujjieying22@163.com>
2025-03-14 13:15:59 +0800

    渲染循环 glad初始化 事件处理

--- FirstOpenGL/main.cpp
+++ FirstOpenGL/main.cpp
@@ -2,13 +2,26 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+// 回调函数：窗口大小改变时，视口大小也随之改变
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+// 处理输入
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
 int main() {
+	// GLFW初始化
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	//创建窗口对象
+	// 创建窗口对象
 	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
 	if (window == NULL) {
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -16,5 +29,27 @@ int main() {
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
+
+	// 初始化GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	// 渲染
+	//glViewport(0, 0, 600, 300); // 视口大小
+	// 添加回调 ：视口大小随着窗口大小改变而改变
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// 渲染循环
+	while (!glfwWindowShouldClose(window)) {
+		processInput(window);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// 清理并退出
+	glfwTerminate();
 	return 0;
 }
