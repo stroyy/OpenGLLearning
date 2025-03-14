b015932d73bd409f155509f2e9e0fe39e0b6cc35
yu <yujjieying22@163.com>
2025-03-14 10:58:37 +0800

    创建窗口对象

--- FirstOpenGL/main.cpp
+++ FirstOpenGL/main.cpp
@@ -1,10 +1,20 @@
 #include <glad/glad.h> 
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main() {
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	//创建窗口对象
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
 	return 0;
 }
